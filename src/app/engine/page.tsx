be7e760d1abc6f0e80f627fdf760a87a2d8c45ba
Aryan Gupta <aryan.2007.guptta@gmail.com>
2026-03-13 00:00:42 +0530

    remove auth blocking engine

--- src/app/engine/page.tsx
+++ src/app/engine/page.tsx
@@ -1,18 +1,5 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 import ClientEngine from "./ClientEngine";
 
-export default async function EnginePage() {
-    const session = await getServerSession(authOptions);
-
-    if (!session) {
-        redirect("/api/auth/signin");
-    }
-
-    return (
-        <>
-            <ClientEngine />
-        </>
-    );
+export default function EnginePage() {
+  return <ClientEngine />;
 }
